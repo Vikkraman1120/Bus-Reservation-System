09087c4c9c4f813451d4628f5f1ac9568981da6b
Vikkraman1120 <vikkraman09@gmail.com>
2024-01-31 09:28:03 +0530

    Adding ConnectionDB file'

---
+++ ConnectionDb.java
@@ -0,0 +1,6 @@
+public class ConnectionDb {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/reservation";
+	static final String user = "root";
+	static final String password = "Vikkrish@1120.";
+}
